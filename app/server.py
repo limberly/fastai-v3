ddf186fd8ee201dc130cc817eabfe7990b26d1df
Hyungue Lim <40773528+limberly@users.noreply.github.com>
2020-02-05 19:54:24 -0500

    Updated for Mushroom Classification
    10 most common mushroom classification

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-6JzuK82ruKs7roQKvXJxElZNpTbmuKO'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['chanterelle', 'crimini', 'enoki', 'morel', 'oyster', 'porcini', 'portabella', 'shiitake', 'shimeji', 'white-button']
 path = Path(__file__).parent
 
 app = Starlette()
